a1f2023278ae136f161dffac5f2e7cac78f5f8ed
Virtya <nik.zheltikov.26@gmail.com>
2021-12-20 18:29:19 +0300

    third lab commit

--- src/main/java/com/example/comservices/repository/ContractRepository.java
+++ src/main/java/com/example/comservices/repository/ContractRepository.java
@@ -5,5 +5,5 @@ import com.example.comservices.entity.Individual;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractRepository extends JpaRepository<Contract,Long> {
-    boolean existsByIndividual(Individual individual);
+    boolean existsByNumber(Long number);
 }
--- src/main/java/com/example/comservices/repository/IndebtednessRepository.java
+++ src/main/java/com/example/comservices/repository/IndebtednessRepository.java
@@ -5,5 +5,5 @@ import com.example.comservices.entity.Individual;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndebtednessRepository extends JpaRepository<Indebtedness,Long> {
-    boolean existsByIndividual(Individual individual);
+    boolean existsByNumber(Long number);
 }
